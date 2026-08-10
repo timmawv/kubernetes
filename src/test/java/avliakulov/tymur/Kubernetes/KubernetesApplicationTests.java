7770fe0d30c6837800331cb935bddc7a1105647c
timuravlyakulov <avlyakulovtimur@gmail.com>
2026-08-10 17:13:30 +0200

    Initial commit

---
+++ src/test/java/avliakulov/tymur/Kubernetes/KubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package avliakulov.tymur.Kubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
